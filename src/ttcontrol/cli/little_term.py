378b5d9f82ce0e6bc4b1db27bee805328cb48bb8
splinedrive <hirosh@dabui.de>
2026-01-13 19:53:17 +0100

    Disable REPL interrupt and forward keyboard input to UART
    Enable Unix job control support

--- src/ttcontrol/cli/little_term.py
+++ src/ttcontrol/cli/little_term.py
@@ -1,18 +1,28 @@
-import uselect
-import sys
+import micropython, ttboard, uselect, sys, utime
 from machine import UART
 
+micropython.kbd_intr(-1)
 
 uart = UART(0, baudrate=115200, tx=tt.pins.ui_in3.raw_pin, rx=tt.pins.uo_out4.raw_pin)
+
 poll = uselect.poll()
 poll.register(sys.stdin, uselect.POLLIN)
 
-
 while True:
-    if poll.poll(0):
-        _ = uart.write(sys.stdin.buffer.read(1))
-    uart_data = uart.read()
-    if uart_data:
-        _ = sys.stdout.write(uart_data)
+    try:
+        try:
+            if poll.poll(0):
+                b = sys.stdin.buffer.read(1)
+                if b:
+                    _ = uart.write(b)
+        except Exception:
+            pass
 
+        d = uart.read()
+        if d:
+            _ = sys.stdout.write(d)
 
+        utime.sleep_ms(2)
+
+    except Exception:
+        utime.sleep_ms(20)
