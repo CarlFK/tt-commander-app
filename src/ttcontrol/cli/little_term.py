3c531fe0fcb6bc8d88d99653530dc2f30b223fae
Carl Karsten <carl@personnelware.com>
2026-01-18 16:31:42 -0600

    little_term.py now breaks on ^c^c.

--- src/ttcontrol/cli/little_term.py
+++ src/ttcontrol/cli/little_term.py
@@ -1,28 +1,72 @@
 import micropython, ttboard, uselect, sys, utime
 from machine import UART
 
-micropython.kbd_intr(-1)
+def little_term( tt, window_time=400*1000000):
 
-uart = UART(0, baudrate=115200, tx=tt.pins.ui_in3.raw_pin, rx=tt.pins.uo_out4.raw_pin)
 
-poll = uselect.poll()
-poll.register(sys.stdin, uselect.POLLIN)
+    # don't halt on ^c (or any other char)
+    micropython.kbd_intr(-1)
 
-while True:
-    try:
+    # get ready to halt on 2 ^c in the window
+    last_char,last_time = None,None
+
+    uart = UART(0, baudrate=115200, tx=tt.pins.ui_in3.raw_pin, rx=tt.pins.uo_out4.raw_pin)
+
+    poll = uselect.poll()
+    poll.register(sys.stdin, uselect.POLLIN)
+
+    while True:
         try:
-            if poll.poll(0):
-                b = sys.stdin.buffer.read(1)
-                if b:
-                    _ = uart.write(b)
-        except Exception:
-            pass
+            try:
+                if poll.poll(0):
 
-        d = uart.read()
-        if d:
-            _ = sys.stdout.write(d)
+                    b = sys.stdin.buffer.read(1)
 
-        utime.sleep_ms(2)
+                    if b:
 
-    except Exception:
-        utime.sleep_ms(20)
+                        # if ^c
+                        if b==b'\x03':
+                            # if first time here, skip check for ^c^c.
+                            if last_char is not None and \
+                               last_char == b and \
+                               utime.time_ns()-last_time < window_time:
+                                   raise KeyboardInterrupt
+
+                        last_char,last_time = b,utime.time_ns()
+
+                        # send the char to uart
+                        uart.write(b)
+
+            except Exception as e:
+                print(f"{e=}")
+                pass
+
+            d = uart.read()
+            if d:
+                sys.stdout.write(d)
+
+            utime.sleep_ms(2)
+
+        except Exception as e:
+            print(f"{e=}")
+            utime.sleep_ms(20)
+
+def go():
+
+    import main
+    tt = main.DemoBoard.get()
+
+    print("calling little_term(tt, window_time=400*1000000)")
+    little_term(tt, window_time=400*1000000)
+
+
+if __name__=="__main__":
+    go()
+else:
+    print("little_term.go()")
+
+    # little_term(tt, True) # NameError: name 'tt' isn't defined
+    print("little_term.little_term(tt, window_time=400*1000000)")
+
+
+
